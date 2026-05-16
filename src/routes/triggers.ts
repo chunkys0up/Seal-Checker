92413d39738b3677c89b83381a5733ec1f58a5f4
Patheit <andrewtnguyen0106@gmail.com>
2026-05-16 12:05:10 -0700

    started working on on post submit

--- src/routes/triggers.ts
+++ src/routes/triggers.ts
@@ -1,8 +1,10 @@
 import { Hono } from 'hono';
 import type {
   OnAppInstallRequest,
+  OnPostSubmitRequest,
   TriggerResponse,
 } from '@devvit/web/shared';
+import { settings } from '@devvit/web/server';
 
 export const triggers = new Hono();
 
@@ -16,4 +18,21 @@ triggers.post('/on-app-install', async (c) => {
     },
     200
   );
+});
+
+triggers.post('/on-post-submit', async (c) => {
+  const input = await c.req.json<OnPostSubmitRequest>();
+
+  const exemptFlairs = (await settings.get<string>('exemptFlairs'))?.split(',');
+  const timeLimit = await settings.get<number>('timeLimit');
+  const actionOnExpiry = await settings.get<string>('actionOnExpiry');
+
+  const postFlair = input.post?.linkFlair?.text;
+
+  // if the post has an exempt flair then skip it
+  if (exemptFlairs && postFlair && exemptFlairs.includes(postFlair)) {
+    return c.json<TriggerResponse>({ status: 'ok' });
+  }
+
+  
 });
